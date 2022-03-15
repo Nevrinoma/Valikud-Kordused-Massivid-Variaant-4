85d2432ac0b4daeffd406935699e8337ac65d874
GIULIANO MARCO <giuliano.lember@gmail.com>
2022-03-16 01:31:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valikud__Kordused__Massivid_Variaant_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            while (true)
+            {
+                Console.WriteLine("1 >>> Palindrom\n2 >>> Matrica\n3 >>> stroka");
+                int choice = int.Parse(Console.ReadLine());
+                if (choice == 1)
+                {
+                    funks.Palidrom();
+                }
+                if (choice == 2)
+                {
+                    funks.Palidrom();
+                }
+                if (choice == 3)
+                {
+                    funks.Palidrom();
+                }
+
+            }
+
+            
+
+        }
+    }
+}
---
+++ funks.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valikud__Kordused__Massivid_Variaant_4
+{
+    public class funks
+    {
+        public static void Palidrom() 
+        {
+            string slovop;
+            int s4et = 0;
+            slovop = Console.ReadLine();
+            string[] polin = new string[slovop.Length];
+            for (int i = 0; i < slovop.Length; i++)
+            {
+                if (slovop[i] == ' ')
+                {
+
+                }
+                else
+                {
+                    polin[i] = slovop[i].ToString().ToLower();
+                }
+
+            }
+
+            for (int i = 0; i < polin.Length; i++)
+            {
+                if (polin[i] == polin[polin.Length - 1 - i])
+                {
+                    s4et++;
+                }
+            }
+            if (s4et == polin.Length)
+            {
+                Console.WriteLine("Это палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Это не палиндром");
+            }
+        }
+    }
+}
